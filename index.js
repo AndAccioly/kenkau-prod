f6e8c0072edf3cc96f25d346335a703bf7be6ffc
André <andreacciolyl@hotmail.com>
2023-07-22 18:20:31 -0300

    Correções layout e express

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ const app = express()
 
 app.use(express.static(path.join(__dirname + "/public")))
 
+app.get('/', (req, res) => {
+  res.send('https://www.kenkau.com/');
+});
+
+app.get('/sobre', (req, res) => {
+  res.send('https://www.kenkau.com/sobre');
+});
+
+
+app.get('/cartas/*', (req, res) => {
+  res.send('https://www.kenkau.com/cartas');
+});
+
+app.get('*', (req, res) => {
+  res.send(req);
+});
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
