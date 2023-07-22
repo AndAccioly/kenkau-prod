28c3f652ceb62ee6c0a0d7d433caa58ac5e26a65
André <andreacciolyl@hotmail.com>
2023-07-22 18:28:06 -0300

    Tentativa de correcao express

--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const app = express()
 
 app.use(express.static(path.join(__dirname + "/public")))
 
-app.get('/', (req, res) => {
-  res.send('https://www.kenkau.com/');
-});
-
 app.get('/sobre', (req, res) => {
-  res.send('https://www.kenkau.com/sobre');
+  res.render('https://www.kenkau.com/sobre');
 });
 
 
-app.get('/cartas/*', (req, res) => {
-  res.send('https://www.kenkau.com/cartas');
+app.get('/cartas', (req, res) => {
+   res.sendFile(path.join(__dirname+'/index.js'));
 });
 
 app.get('*', (req, res) => {
