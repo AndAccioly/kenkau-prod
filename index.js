23fc716ded13acbd019dee8bb67ab11ec764d4da
André <andreacciolyl@hotmail.com>
2023-07-22 21:34:32 -0300

    Tentativa de correcao

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ app.get('/sobre', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// app.get('/cartas', (req, res) => {
-//   console.log(req)
-//   res.send('Hello World!')
-// });
+app.get('/cartas', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
