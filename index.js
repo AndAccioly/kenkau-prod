3b5d0151ca0c41f942159a7999340a424eecf0e3
André Accioly <andreacciolyl@hotmail.com>
2024-05-10 21:24:38 -0300

    worker

--- index.js
+++ index.js
@@ -82,7 +82,9 @@ app.get('/semAcesso', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('*', (req, res) => {
     res.redirect('/')
