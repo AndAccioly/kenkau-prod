03128651e3ef05b28a5bd62d3df6aad653e66be1
André Accioly <andreacciolyl@hotmail.com>
2024-08-02 23:37:58 -0300

    att

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ app.get('/semAcesso', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/politica', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/termos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('*', (req, res) => {
     res.redirect('/')
 });
